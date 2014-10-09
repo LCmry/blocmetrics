cdaced62ae5ee46c4b234d95502b842a2106898c
LCmry <lcrees.2014@gmail.com>
2014-10-09 10:56:50 -0700

    Adding create for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,16 @@
 class EventsController < ApplicationController
- 
+ respond_to :json
+
+ def create
+  @event = Event.new(event_params)
+  if @event.save
+    render json: @event
+  else
+    render json: @event.errors.full_messages
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:site_id)
 end
