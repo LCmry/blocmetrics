2d0ac6a7cfcbd7f280f48f10ae5df41a9e3bf293
LCmry <lcrees.2014@gmail.com>
2014-10-09 10:39:31 -0700

    Added route for CORS options

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,11 +1,11 @@
 class Api::BaseController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_filter :set_headers
 
   private
 
   def set_headers
     headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Expose-Headers'] = 'ETag'
     headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
     headers['Access-Control-Allow-Headers'] = 'Content-Type'
     headers['Access-Control-Max-Age'] = '1728000'
