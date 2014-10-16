7ad5f27e9f7c8349c7c761f90d44934967d1e8ce
LCmry <lcrees.2014@gmail.com>
2014-10-16 11:25:27 -0700

    Added new graph, making views better

--- app/assets/javascripts/sites.js
+++ app/assets/javascripts/sites.js
@@ -1,67 +1,78 @@
 $( document ).ready(function() {
-    var ctx = document.getElementById("lineChart").getContext("2d");
-    //var views = new Array();
-    //console.log(<%= @events %>);
-    //views.push(<%= @events %>.to_json);
-    //var dates = new Array();
-    //dates.push(<%= @dates %>.to_json);
-    var data = {
-        labels: dates,
-        datasets: [
-            {
-                label: "Site Views",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: views
-            }
-        ]
-    };
-    var options = {
+	var ctx1 = document.getElementById("lineChart1").getContext("2d");
+	var data1 = {
+		labels: dates1,
+		datasets: [
+			{
+				label: "Site Views",
+				fillColor: "rgba(220,220,220,0.2)",
+				strokeColor: "rgba(220,220,220,1)",
+				pointColor: "rgba(220,220,220,1)",
+				pointStrokeColor: "#fff",
+				pointHighlightFill: "#fff",
+				pointHighlightStroke: "rgba(220,220,220,1)",
+				data: views1
+			}
+		]
+	};
+	var ctx2 = document.getElementById("lineChart2").getContext("2d");
+	var data2 = {
+		labels: dates2,
+		datasets: [
+			{
+				label: "Site Views",
+				fillColor: "rgba(220,220,220,0.2)",
+				strokeColor: "rgba(220,220,220,1)",
+				pointColor: "rgba(220,220,220,1)",
+				pointStrokeColor: "#fff",
+				pointHighlightFill: "#fff",
+				pointHighlightStroke: "rgba(220,220,220,1)",
+				data: views2
+			}
+		]
+	};
+	var options = {
 
-        ///Boolean - Whether grid lines are shown across the chart
-        scaleShowGridLines : true,
+		///Boolean - Whether grid lines are shown across the chart
+		scaleShowGridLines : true,
 
-        //String - Colour of the grid lines
-        scaleGridLineColor : "rgba(0,0,0,.05)",
+		//String - Colour of the grid lines
+		scaleGridLineColor : "rgba(0,0,0,.05)",
 
-        //Number - Width of the grid lines
-        scaleGridLineWidth : 1,
+		//Number - Width of the grid lines
+		scaleGridLineWidth : 1,
 
-        //Boolean - Whether the line is curved between points
-        bezierCurve : true,
+		//Boolean - Whether the line is curved between points
+		bezierCurve : true,
 
-        //Number - Tension of the bezier curve between points
-        bezierCurveTension : 0.4,
+		//Number - Tension of the bezier curve between points
+		bezierCurveTension : 0.4,
 
-        //Boolean - Whether to show a dot for each point
-        pointDot : true,
+		//Boolean - Whether to show a dot for each point
+		pointDot : true,
 
-        //Number - Radius of each point dot in pixels
-        pointDotRadius : 4,
+		//Number - Radius of each point dot in pixels
+		pointDotRadius : 4,
 
-        //Number - Pixel width of point dot stroke
-        pointDotStrokeWidth : 1,
+		//Number - Pixel width of point dot stroke
+		pointDotStrokeWidth : 1,
 
-        //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-        pointHitDetectionRadius : 20,
+		//Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+		pointHitDetectionRadius : 20,
 
-        //Boolean - Whether to show a stroke for datasets
-        datasetStroke : true,
+		//Boolean - Whether to show a stroke for datasets
+		datasetStroke : true,
 
-        //Number - Pixel width of dataset stroke
-        datasetStrokeWidth : 2,
+		//Number - Pixel width of dataset stroke
+		datasetStrokeWidth : 2,
 
-        //Boolean - Whether to fill the dataset with a colour
-        datasetFill : false,
+		//Boolean - Whether to fill the dataset with a colour
+		datasetFill : false,
 
-        //String - A legend template
-        legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+		//String - A legend template
+		legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
 
-    };
-    var myLineChart = new Chart(ctx).Line(data, options);
-    //myLineChart.addData(views, dates);
+	};
+	var myLineChart1 = new Chart(ctx1).Line(data1, options);
+	var myLineChart2 = new Chart(ctx2).Line(data2, options);
 });
